5d57b3d8fda853c34845c85d04c7593db3fd22c1
thanhvyhcmus <thanhvy.forwork@gmail.com>
2021-03-22 20:41:34 +0700

    add tab layout

--- app/src/main/java/com/example/oldstocktrade/HistoryFragment.java
+++ app/src/main/java/com/example/oldstocktrade/HistoryFragment.java
@@ -8,11 +8,18 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class HistoryFragment extends Fragment {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPageAdapter adapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_history, container, false);
+        View view =  inflater.inflate(R.layout.fragment_history, container, false);
+        return view;
     }
 }
