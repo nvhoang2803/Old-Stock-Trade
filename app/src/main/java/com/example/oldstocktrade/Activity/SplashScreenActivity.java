9f71ad92252fa7477cf354cb37479c1fc0853a0c
thanhvyhcmus <thanhvy.forwork@gmail.com>
2021-05-15 01:48:18 +0700

    SplashScreen

---
+++ app/src/main/java/com/example/oldstocktrade/Activity/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.example.oldstocktrade.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.oldstocktrade.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
